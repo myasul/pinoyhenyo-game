605fbcd47065c105fd5bf27caf2094a676b1c553
Matthew Yasul <yasulmatthew@gmail.com>
2025-04-18 12:19:10 +1200

    Change global font style

--- app/frontend/src/app/layout.tsx
+++ app/frontend/src/app/layout.tsx
@@ -1,15 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Unkempt } from "next/font/google";
 import "../styles/globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const unkempt = Unkempt({
+  weight: ["400", "700"],
+  style: "normal",
+  subsets: ["latin"]
 });
 
 export const metadata: Metadata = {
@@ -25,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased h-dvh`}
+        className={`${unkempt.className} antialiased h-dvh`}
       >
         {children}
       </body>
