a45cb7f487ec57f5e31e4484ccd8119ce317742e
Matthew Yasul <yasulmatthew@gmail.com>
2025-05-06 12:24:30 +1200

    Update dockerfile and fly.toml

--- app/frontend/src/app/duo/[gameId]/lobby/components/LobbyNewJoiner.tsx
+++ app/frontend/src/app/duo/[gameId]/lobby/components/LobbyNewJoiner.tsx
@@ -1,7 +1,9 @@
 import { Footer } from '@/components/Footer';
 import { PageLayout } from '@/components/PageLayout';
+import { WaveButton } from '@/components/WaveButton';
 import { useState } from 'react';
 import { LogIn } from 'react-feather';
+import { adjectives, animals, uniqueNamesGenerator } from 'unique-names-generator';
 
 type Props = {
     onJoin: (playerName: string) => void;
@@ -11,10 +13,26 @@ type Props = {
 export default function LobbyNewJoiner({ onJoin, onExit }: Props) {
     const [name, setName] = useState('');
 
+    const handleKeyDown = (event: React.KeyboardEvent) => {
+        if (event.key === 'Enter') {
+            onJoin(name.trim());
+        }
+    };
+
+    const handleRandomNameBtnClick = () => {
+        const randomName = uniqueNamesGenerator({
+            dictionaries: [adjectives, animals],
+            separator: '-',
+            length: 2
+        })
+
+        setName(randomName);
+    }
+
     return (
         <PageLayout>
             <section className="flex flex-col items-center gap-3">
-                <h2 className="text-lg font-semibold text-gray-800">
+                <h2 className="text-2xl font-semibold text-gray-800">
                     Please enter your nickname:
                 </h2>
 
@@ -23,8 +41,12 @@ export default function LobbyNewJoiner({ onJoin, onExit }: Props) {
                     value={name}
                     onChange={(e) => setName(e.target.value)}
                     placeholder="Enter your nickname here"
-                    className="w-full px-4 py-2 rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    className="w-full px-4 py-2 rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 text-2xl"
+                    onKeyDown={handleKeyDown}
                 />
+                <WaveButton onClick={handleRandomNameBtnClick} className=''>
+                    Generate random name
+                </WaveButton>
             </section>
             <Footer
                 onBack={onExit}
