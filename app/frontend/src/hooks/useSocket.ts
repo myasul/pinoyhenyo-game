61e32b0952fa940b4af24e8a96fe76ef6d43f0fe
Matthew Yasul <yasulmatthew@gmail.com>
2025-05-16 19:13:48 +1200

    Simplify useSocket

--- app/frontend/src/hooks/useSocket.ts
+++ app/frontend/src/hooks/useSocket.ts
@@ -2,7 +2,7 @@ import io from "socket.io-client"
 
 let socketInstance: typeof io.Socket | null = null
 
-export const getSocket = () => {
+export const useSocket = () => {
     const socketUrl = process.env.NEXT_PUBLIC_SOCKET_URL || 'http://localhost:3001'
 
     if (!socketInstance) {
@@ -13,49 +13,10 @@ export const getSocket = () => {
         })
     }
 
-    return socketInstance
-}
-
-export const useSocket = () => {
-    // const isExplicityDisconnected = useRef(false)
-
-
-    // const socket = getSocket()
-
-    // useEffect(() => {
-    //     const socket = getSocket()
-
-    //     // if (socket.connected) setIsConnected(true)
-
-    //     // socket.on(SocketEvent.Connect, () => {
-    //     //     console.log('Connected to socket server! Socket ID:', socket.id)
-
-    //     //     setIsConnected(true)
-    //     // })
-    //     // socket.on(SocketEvent.Disconnect, () => {
-    //     //     console.log('Disconnected from socket server! Socket ID:', socket.id)
-
-    //     //     setIsConnected(false)
-    //     //     socket.connect()
-    //     // })
-
-    //     return () => {
-    //         socket.off(SocketEvent.Connect)
-    //         socket.off(SocketEvent.Disconnect)
-
-    //         if (!isExplicityDisconnected.current) return
-
-    //         console.log('Explicitly disconnected. Cleaning up socket connection...')
-
-    //         socket.disconnect()
-    //         socketInstance = null
-    //     }
-    // }, [])
-
     const disconnectSocket = () => {
         socketInstance?.disconnect()
         socketInstance = null
     }
 
-    return { disconnectSocket, socket: getSocket() }
+    return { disconnectSocket, socket: socketInstance }
 }
